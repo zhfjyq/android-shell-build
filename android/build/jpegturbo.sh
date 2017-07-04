adfccbde343ae18e4c10933cddb9cffe59b02d9a
zhufeng <zhfjyq@163.com>
2017-07-04 23:00:43 +0800

    OK

---
+++ android/build/jpegturbo.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+#set -xe
+
+MEDIR=$(cd `dirname $0`; pwd)
+ME=jpegturbo
+
+cd $MEDIR
+source env.sh
+source common.sh
+
+# build libjpeg-turbo
+cd ../$ME/libjpeg-turbo;pwd
+#./configure $CONFIGFLAGS --prefix=$MEDIR/../$ME/dist/
+
+#LDFLAGS="$LDFLAGS --with-simd"
+#make
+
+#build app jpegturbo
+cd ..
+pwd
+echo
+$CC $CPPFLAGS ./jpegturbo.c -L./libjpeg-turbo/.libs -l turbojpeg -o ./jpegturbo
